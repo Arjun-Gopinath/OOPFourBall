99c5826bbcbb633aad4e3204bdb10b072948a1ef
Arjun-Gopinath <arjungopinath06@gmail.com>
2021-08-02 20:13:38 +0530

    removed code smells

--- src/Ball.java
+++ src/Ball.java
@@ -2,20 +2,20 @@ import processing.core.PApplet;
 
 public class Ball extends PApplet {
 
-    private final int height;
+    private final int HEIGHT;
     private int positionX;
     private int speed;
     private final int DIAMETER;
 
     public Ball(int speed, int height) {
-        this.height = height;
+        this.HEIGHT = height;
         this.speed = speed;
         this.DIAMETER = 10;
         this.positionX = 0;
     }
     
     public void ball(PApplet pApplet) {
-        pApplet.ellipse(positionX,height,DIAMETER,DIAMETER);
+        pApplet.ellipse(positionX, HEIGHT,DIAMETER,DIAMETER);
     }
 
     public void newPosition() {
